befc02a600abb722a62831ba60a19ab230f7f840
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2023-10-24 14:44:36 +0100

    Final Project

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -16,19 +16,6 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-	@Configuration
-	public class CorsConfig {
-		@Bean
-		public CorsConfigurationSource corsConfigurationSource() {
-			CorsConfiguration configuration = new CorsConfiguration();
-			configuration.addAllowedOrigin("http://localhost:3000"); // Replace with your React app's origin
-			configuration.addAllowedMethod("*");
-			configuration.addAllowedHeader("*");
 
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			source.registerCorsConfiguration("/**", configuration);
-			return source;
-		}
-	}
 
 }
